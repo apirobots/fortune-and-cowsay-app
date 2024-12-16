9376150ca612967ad78579c1f50852e05f49ad92
Borislav Gizdov <borislav.gizdov@gmail.com>
2024-12-16 21:54:51 +0200

    Added netlify deploy

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,16 +1,24 @@
 const API_BASE = 'https://fortune-and-cowsay-api-by-apirobots.p.rapidapi.com/v1';
 
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPIDAPI_KEY || '',
+    'X-RapidAPI-Host': process.env.NEXT_PUBLIC_RAPIDAPI_HOST || ''
+  }
+};
+
 export const getFortune = async () => {
-  const response = await fetch(`${API_BASE}/fortune/plain`);
+  const response = await fetch(`${API_BASE}/fortune/plain`, options);
   return response.json();
 };
 
 export const getCowsay = async () => {
-  const response = await fetch(`${API_BASE}/fortune/cowsay`);
+  const response = await fetch(`${API_BASE}/fortune/cowsay`, options);
   return response.json();
 };
 
 export const getRandomCow = async () => {
-  const response = await fetch(`${API_BASE}/fortune/random-cow`);
+  const response = await fetch(`${API_BASE}/fortune/random-cow`, options);
   return response.json();
 };
